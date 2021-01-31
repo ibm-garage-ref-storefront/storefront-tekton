4a83c4f36fa1c702c1228da9533bff84c8d38cf2
ronald van de kuil <ronald@oc2835351741.ibm.com>
2021-01-31 09:14:43 +0100

    expose customer-ms-spring

--- scripts/web/deploy.sh
+++ scripts/web/deploy.sh
@@ -33,10 +33,11 @@ do
             sed -i "s/auth-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json  
 
             # do the same for customer-ms-spring
-            #export ROUTE=$(oc get route | grep customer-ms-spring | awk  '{ print $2}')
-            #echo "ROUTE=>$ROUTE<"
+            echo "searching for route to customer-ms-spring service"
+            export ROUTE=$(oc get route | grep customer-ms-spring | awk  '{ print $2}')
+            echo "ROUTE=>$ROUTE<"
 
-            #sed -i "s/customer-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json  
+            sed -i "s/customer-ms-spring:8080/$ROUTE/" $HERE/scripts/web/production.json  
 
             break
             ;;
