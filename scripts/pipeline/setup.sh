3469ebfb6a65594e5732e25cb0be63cb097dda3e
kitty-catt <ronald@oc2835351741.ibm.com>
2021-03-14 13:04:36 +0100

    run on internal registry

--- scripts/pipeline/setup.sh
+++ scripts/pipeline/setup.sh
@@ -16,6 +16,7 @@ oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/skope
 oc apply -f tekton-tasks/ibm-kabanero-java-test.yaml 
 oc create -f tekton-tasks/kabanero-spring-boot2.yaml
 oc create -f https://raw.githubusercontent.com/IBM/ibm-garage-tekton-tasks/main/tasks/3-img-scan-trivy.yaml
+oc apply -f tekton-tasks/aot-mockup.yaml 
 
 # the official one does not work on Feb 28.
 # https://github.com/IBM/ibm-garage-tekton-tasks/issues/98
