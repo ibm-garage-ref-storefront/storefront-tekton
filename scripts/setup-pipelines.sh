cb4b188308ccc6be69fcb7980e6840f623f460c5
kitty-catt <ronald@oc2835351741.ibm.com>
2021-03-14 14:26:23 +0100

    working push

--- scripts/setup-pipelines.sh
+++ scripts/setup-pipelines.sh
@@ -43,3 +43,6 @@ oc create is customer -n full-bc
 oc policy add-role-to-user system:image-puller developer
 
 oc project pipelines
+oc policy add-role-to-user edit system:serviceaccount:pipelines:appsody-sa
+oc policy add-role-to-user admin system:serviceaccount:pipelines:appsody-sa
+oc policy add-role-to-user admin system:serviceaccount:pipelines:pipeline
