298e467192d43fafd7c8f234bc9904753fd0ef74
kitty-catt <kitty-catt@example.com>
2021-05-03 09:44:47 +0200

    create access key to sonarqube

--- scripts/pipeline/mini-setup.sh
+++ scripts/pipeline/mini-setup.sh
@@ -22,6 +22,12 @@ oc apply -f pvc --recursive
 
 #oc secret link pipeline crc-creds-skopeo
 
+oc delete secret sonarqube-access 2>/dev/null
+oc create secret generic sonarqube-access \
+    --from-literal SONARQUBE_PROJECT="GENERIC-PROJECT" \
+    --from-literal SONARQUBE_URL='http://sonarqube-sonarqube.tools.svc.cluster.local:9000' \
+    --from-literal SONARQUBE_LOGIN=${SONAR_QUBE_PAT}
+
 oc policy add-role-to-user system:image-pusher system:serviceaccount:full-bc:pipeline
 oc adm policy add-scc-to-user privileged system:serviceaccount:pipelines:pipeline
 
