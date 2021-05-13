7d8aa2231d13f09693f551c06b38bd269a55c05c
kitty-catt <kitty-catt@example.com>
2021-05-13 11:35:01 +0200

    add performance tooling

--- tools/setup.sh
+++ tools/setup.sh
@@ -39,9 +39,11 @@ echo ""
 # Setup jmeter-performance-test
 echo "###################################################################"
 cd ${HERE}/tools/jmeter-performance-test
-./jmeter-performance-test/build_jmeter_image.sh  
-./jmeter-performance-test/install_jmeter_framework.sh
+./build_jmeter_image.sh  
+./install_jmeter_framework.sh
 echo ""
 
 # view cannot create pods/portforward
 oc policy add-role-to-user admin ${OCP_USER}
+
+cd ${HERE}
