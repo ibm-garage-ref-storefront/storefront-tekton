4258b2331ddf28abe1a5500b9c98ee954579a3a9
kitty-catt <kitty-catt@example.com>
2021-05-23 11:00:53 +0200

    add sonar qube

--- tools/sonarqube/port-forward.sh
+++ tools/sonarqube/port-forward.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+clear
+
 SQ=$(oc get po -n tools | grep sonarqube-sonarqube | cut -f1 -d" ")
 echo "Use the following command to setup port-forwarding to sonarqube:"
 echo "oc port-forward $SQ -n tools 9000:9000&"
