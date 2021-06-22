93f9049f371f2c8232a317c6dc96302a6b79482b
kitty-catt <kitty-catt@example.com>
2021-06-22 13:12:07 +0200

    add stackrox access secret

--- scripts/pipeline/stackrox-setup.sh
+++ scripts/pipeline/stackrox-setup.sh
@@ -25,14 +25,20 @@ oc apply -f tekton-tasks/aot-stackrox-task.yaml
 echo "--------------------------------"
 echo "add secrets:"
 
+# Access to Openshift
 export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
-
 oc delete secret generic oir-registrycreds 2>/dev/null
-
 oc create secret generic oir-registrycreds \
 --from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
 --type kubernetes.io/dockerconfigjson
+
+# Access to StackRox
+oc delete secret generic stackrox-details 2>/dev/null
+cp tools/stackrox/stackrock-details.yaml /tmp/stackrock-details.yaml
+sed -i "s/REPLACE_WITH_STACKROX_API_TOKEN/${STACKROX_API_TOKEN}/g" /tmp/stackrock-details.yaml
+oc apply -f /tmp/stackrock-details.yaml
+rm /tmp/stackrock-details.yaml
 
 echo "--------------------------------"
 
