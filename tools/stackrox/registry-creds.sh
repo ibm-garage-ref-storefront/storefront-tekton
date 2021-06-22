1509f7cb22e7febafda7577fe7edb6517f2c7e5b
kitty-catt <kitty-catt@example.com>
2021-06-22 11:48:28 +0200

    create ior registry secret for stackrox scanner

--- tools/stackrox/registry-creds.sh
+++ tools/stackrox/registry-creds.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
 
-clear
-
-source ~/config.bc-full
-
-oc project pipelines
-
-oc apply -f tekton-resources/tools-images/ubi-image.yaml
-
-
 export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
 
@@ -16,12 +7,8 @@ docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
 #--from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
 #--type kubernetes.io/dockerconfigjson
 
-oc delete secret generic oir-registrycreds 2>/dev/null
-
 oc create secret generic oir-registrycreds \
 --from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
 --type kubernetes.io/dockerconfigjson
 
-#oc extract secret/oir-registrycreds --to=-
-
-
+oc extract secret/oir-registrycreds --to=-
