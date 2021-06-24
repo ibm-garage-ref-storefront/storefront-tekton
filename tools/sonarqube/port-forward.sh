b91a689e54748acdab31adf87273f0f2765bf3b7
kitty-catt <kitty-catt@example.com>
2021-06-24 07:48:32 +0200

    failsave

--- tools/sonarqube/port-forward.sh
+++ tools/sonarqube/port-forward.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-SQ=$(oc get po -n tools | grep sonarqube-sonarqube | cut -f1 -d" ")
+SQ=$(oc get po -n tools | grep sonarqube-sonarqube | awk '{ print $1 }')
 echo "Use the following command to setup port-forwarding to sonarqube:"
 echo "oc port-forward $SQ -n tools 9000:9000&"
 
