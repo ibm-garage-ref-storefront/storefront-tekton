34ce520c2cd6fbe9e41d96205c5edf11d4857d08
kitty-catt <kitty-catt@example.com>
2021-07-02 13:20:36 +0200

    initial state check

--- tools/stackrox/secure-cluster-services.sh
+++ tools/stackrox/secure-cluster-services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 if [ -f ~/bin/roxctl ] 
 then
     echo "stackrox cli allready exists"
@@ -8,8 +10,20 @@ else
     curl https://mirror.openshift.com/pub/rhacs/assets/3.0.62.0/bin/Linux/roxctl -o ~/bin/roxctl     
     chmod 755 ~/bin/roxctl
 fi
+echo ""
 
 echo "stackrox state:"
 oc get po -n stackrox
+echo ""
+
+SR_STATE=$(oc get po -n stackrox | grep Running | wc -l)
+
+if [ ${SR_STATE} -eq 4 ] ; then
+    echo "StackRox is up"
+else
+    echo "StackRox initial setup is not complete yet, please try again later"
+fi
+echo ""
+
 
 helm get notes stackrox-central-services -n stackrox | grep -A3 "initial setup"
