963daa5b8ca53241008002df4a62c509046b9448
kitty-catt <kitty-catt@example.com>
2021-07-02 13:51:58 +0200

    script the cluster monitoring

--- tools/stackrox/secure-cluster-services.sh
+++ tools/stackrox/secure-cluster-services.sh
@@ -26,13 +26,25 @@ fi
 echo ""
 
 echo "start port-forward to stackrox"
-oc port-forward svc/central -n stackrox 8443:443&
-sleep 5
+oc port-forward svc/central -n stackrox 8443:443 > /dev/null 2>&1 &
 echo ""
+sleep 5
 
 helm get notes stackrox-central-services -n stackrox | grep -A3 "initial setup"
 
-sleep 10
+echo "login to https://localhost:8443 and make the ROX_API_TOKEN”"
+
+echo “Enter ROX_API_TOKEN (role admin)”
+read ROX_API_TOKEN
+echo “ROX_API_TOKEN:” $ROX_API_TOKEN
+
+export ROX_API_TOKEN=$ROX_API_TOKEN
+export ROX_CENTRAL_ADDRESS=localhost:8443
+
+roxctl -e "$ROX_CENTRAL_ADDRESS" central init-bundles generate prevail-cluster --output cluster-init-bundle.yaml --insecure-skip-tls-verify
+
+helm install -n stackrox stackrox-secured-cluster-services rhacs/secured-cluster-services -f cluster-init-bundle.yaml --set centralEndpoint=central.stackrox:443 --set clusterName=prevail-cluster --set imagePullSecrets.allowNone=true
+
 
 # bring tunnel to foreground
 echo "end port-forward to stackrox"
