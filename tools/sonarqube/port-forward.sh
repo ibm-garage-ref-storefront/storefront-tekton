70f938c5a2fcf6c4083b314e8915cbc258498211
kitty-catt <kitty-catt@example.com>
2021-08-22 12:44:07 +0200

    provide second alternative scripted

--- tools/sonarqube/port-forward.sh
+++ tools/sonarqube/port-forward.sh
@@ -10,13 +10,27 @@ echo "oc port-forward $SQ -n tools 9000:9000&"
 echo ""
 
 echo "Or consider lowering the drawbridge:"
-echo "oc expose svc sonarqube-sonarqube"
+
+while true; do
+    read -p "Do you wish to expose sonarqube to the internet (Y/N)?" yn
+    case $yn in
+        [Yy]* ) 
+            oc create route edge --service=sonarqube-sonarqube; 
+            break;;
+        [Nn]* ) 
+            exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
 SQR=$(oc get routes -n tools | grep sonarqube-sonarqube | awk '{ print $2 }')
 echo ""
 echo "Open a browser to:"
-echo "http://${SQR}/"
+echo "https://${SQR}/"
 echo ""
 echo "make the sonarqube PAT .. and ... perhaps you want to consider closing the drawbridge:"
+echo ""
 echo "oc delete route sonarqube-sonarqube"
 
 
