0040c3720eb198ee31b910a8faadafe62259a3cc
kitty-catt <kitty-catt@example.com>
2021-08-22 13:38:16 +0200

    rework the port-forward

--- tools/httpd/port-forward.sh
+++ tools/httpd/port-forward.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-SQ=$(oc get po -n tools | grep silver-platter | grep -v deploy | cut -f1 -d" ")
+#SQ=$(oc get po -n tools | grep silver-platter | grep -v deploy | cut -f1 -d" ")
 echo "Use the following command to setup port-forwarding to the silver platter:"
-echo "oc port-forward $SQ -n tools 8080:8080&"
+echo "oc port-forward svc/silver-platter -n tools 8080:8080&"
 
--- tools/sonarqube/port-forward.sh
+++ tools/sonarqube/port-forward.sh
@@ -4,9 +4,10 @@ clear
 oc project tools
 echo ""
 
-SQ=$(oc get po -n tools | grep sonarqube-sonarqube | awk '{ print $1 }')
+#SQ=$(oc get po -n tools | grep sonarqube-sonarqube | awk '{ print $1 }')
 echo "Use the following command to setup port-forwarding to sonarqube:"
-echo "oc port-forward $SQ -n tools 9000:9000&"
+echo "oc port-forward svc/sonarqube-sonarqube -n tools 9000:9000&"
+#echo "oc port-forward $SQ -n tools 9000:9000&"
 echo ""
 
 echo "Or consider lowering the drawbridge:"
