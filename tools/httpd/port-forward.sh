b6df0225e9d8b42d9b8c5bf3833e9de268302093
kitty-catt <kitty-catt@example.com>
2021-08-22 14:02:29 +0200

    add the drawbridge option

--- tools/httpd/port-forward.sh
+++ tools/httpd/port-forward.sh
@@ -5,3 +5,28 @@ clear
 echo "Use the following command to setup port-forwarding to the silver platter:"
 echo "oc port-forward svc/silver-platter -n tools 8080:8080&"
 
+echo ""
+
+echo "Or consider lowering the drawbridge:"
+
+while true; do
+    read -p "Do you wish to expose the silver-platter to the internet (Y/N)?" yn
+    case $yn in
+        [Yy]* ) 
+            oc create route edge --service=silver-platter; 
+            break;;
+        [Nn]* ) 
+            exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+SILVER=$(oc get routes -n tools | grep silver-platter | awk '{ print $2 }')
+echo ""
+echo "Open a browser to:"
+echo "https://${SILVER}/"
+echo ""
+echo " .. and ... perhaps you want to consider closing the drawbridge when you are done:"
+echo ""
+echo "oc delete route silver-platter"
