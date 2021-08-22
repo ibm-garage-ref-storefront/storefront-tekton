a813d67ef82b1f776c92217598a55248d465df1c
kitty-catt <kitty-catt@example.com>
2021-08-22 12:34:21 +0200

    provide a 2-nd alternative

--- tools/sonarqube/port-forward.sh
+++ tools/sonarqube/port-forward.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 clear
 
+oc project tools
+echo ""
+
 SQ=$(oc get po -n tools | grep sonarqube-sonarqube | awk '{ print $1 }')
 echo "Use the following command to setup port-forwarding to sonarqube:"
 echo "oc port-forward $SQ -n tools 9000:9000&"
+echo ""
+
+echo "Or consider lowering the drawbridge:"
+echo "oc expose svc sonarqube-sonarqube"
+SQR=$(oc get routes -n tools | grep sonarqube-sonarqube | awk '{ print $2 }')
+echo ""
+echo "Open a browser to:"
+echo "http://${SQR}/"
+echo ""
+echo "make the sonarqube PAT .. and ... perhaps you want to consider closing the drawbridge:"
+echo "oc delete route sonarqube-sonarqube"
+
+
 
