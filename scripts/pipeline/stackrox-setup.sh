0c6428686bec05c2bcead33cee111bc160de915b
Matt Colman <33348891+mtcolman@users.noreply.github.com>
2021-09-27 09:21:00 +0100

    Update stackrox-setup.sh

--- scripts/pipeline/stackrox-setup.sh
+++ scripts/pipeline/stackrox-setup.sh
@@ -27,7 +27,7 @@ oc apply -f tekton-tasks/aot-stackrox-task-experimental.yaml
 echo "--------------------------------"
 echo "add secrets:"
 
-# Access to Openshift
+# Access to OpenShift
 export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
 oc delete secret generic oir-registrycreds 2>/dev/null
@@ -37,11 +37,9 @@ oc create secret generic oir-registrycreds \
 
 # Access to StackRox
 oc delete secret generic stackrox-details 2>/dev/null
-cp tools/stackrox/stackrock-details.yaml /tmp/stackrock-details.yaml
-sed -i "s/REPLACE_WITH_STACKROX_API_TOKEN/${STACKROX_API_TOKEN}/g" /tmp/stackrock-details.yaml
-oc apply -f /tmp/stackrock-details.yaml
-rm /tmp/stackrock-details.yaml
+cp tools/stackrox/stackrock-details.yaml /tmp/stackrox-details.yaml
+sed -i "s/REPLACE_WITH_STACKROX_API_TOKEN/${STACKROX_API_TOKEN}/g" /tmp/stackrox-details.yaml
+oc apply -f /tmp/stackrox-details.yaml
+rm /tmp/stackrox-details.yaml
 
 echo "--------------------------------"
-
-
