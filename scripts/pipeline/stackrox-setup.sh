838980a4a9d21d3a4543510631cfb7c2082501af
Matt Colman <33348891+mtcolman@users.noreply.github.com>
2021-10-05 10:21:27 +0100

    Update stackrox-setup.sh

--- scripts/pipeline/stackrox-setup.sh
+++ scripts/pipeline/stackrox-setup.sh
@@ -28,11 +28,40 @@ echo "--------------------------------"
 echo "add secrets:"
 
 # Access to OpenShift
+#export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+#docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
+#oc delete secret generic oir-registrycreds 2>/dev/null
+#oc create secret generic oir-registrycreds \
+#--from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
+#--type kubernetes.io/dockerconfigjson
+
+# Access to OpenShift
 export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
-docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
+docker login -u $(oc whoami) -p $(oc whoami -t) $HOST ### maybe comment this line out, unless something else requires the login to have occurred? ###
+
+# find os to ensure correct base64 command syntax
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    mycommand="base64 -w 0"  
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    mycommand="/usr/bin/base64 -b 0"
+else
+    printf "\nOS-TYPE is not Linux or Mac, so attempting base64 single-line command. Please investigate if it fails.\n"
+    mycommand="base64 -w 0"
+fi
+
+# create auth file
+cat <<EOF > ./auth.json
+{
+        "auths": {
+                "$HOST": {
+                        "auth": "$(echo -n $(oc whoami):$(oc whoami -t) | $mycommand)"
+                }
+        }
+}
+EOF
 oc delete secret generic oir-registrycreds 2>/dev/null
 oc create secret generic oir-registrycreds \
---from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
+--from-file .dockerconfigjson=./auth.json \
 --type kubernetes.io/dockerconfigjson
 
 # Access to StackRox
