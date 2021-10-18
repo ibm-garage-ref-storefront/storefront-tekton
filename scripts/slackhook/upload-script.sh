d1484289961bfc635d740b7794593b4144f676ae
Matt Colman <33348891+mtcolman@users.noreply.github.com>
2021-10-18 13:15:57 +0100

    Create upload-script.sh

---
+++ scripts/slackhook/upload-script.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+clear
+
+if ! oc whoami >> /dev/null 2>&1; then
+  printf "\nPlease ensure you are logged into the cluster, so that this script can run.\n"
+  exit 1
+fi
+
+# Will need to set these correctly for the labs
+pipelinens="pipelines"
+backendns="full-bc"
+
+printf "Creating json file...\n"
+cat <<EOF > ./test.json
+{
+"channel": "C123456",
+"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get backend pods:* $(oc get pod -n $backendns | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get pipeline pods:* $(oc get pod -n $pipelinens | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get backends svc:* $(oc get svc -n $backendns | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get pr:* $(oc get pr -n $pipelinens | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get pipeline configMaps:* $(oc get cm -n $pipelinens | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) oc get pipeline secrets:* $(oc get secret -n $pipelinens | awk '{print $1}' | grep -v "NAME"))"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*$(echo -n $(oc whoami) $(date +%d-%m-%Y) helm list backends:* $(helm list -n $backendns | awk '{print $1}' | grep -v "NAME"))"
+			}
+		}
+	]
+}
+EOF
+
+RC=$?
+
+# Check if file creation failed or not.
+if [ $RC != 0 ]; then
+  printf "\nERROR! The payload json was not successfully created. Please investigate\n"
+  exit 1
+else
+  printf "\nFile creation completed successfully.\n"
+fi
+
+printf "\nAttempting curl...\n"
+response=$(curl -s --write-out '%{http_code}' -H "Content-type: application/json" --data @test.json -X POST https://hooks.slack.com/services/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)
+
+if [ "$response" = "ok200" ]; then
+  printf "\nCurl completed successfully.\n"
+else
+  printf "\nERROR! The curl command failed to complete properly. Please investigate.\n"
+  exit 1
+fi
+
+printf "\nScript complete, goodbye.\n"
