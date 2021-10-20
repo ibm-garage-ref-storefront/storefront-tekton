043810d3ee84994d98e185217471c8dad6d47ce5
Matt Colman <33348891+mtcolman@users.noreply.github.com>
2021-10-20 16:23:28 +0100

    Update registry-creds.sh

--- tools/stackrox/registry-creds.sh
+++ tools/stackrox/registry-creds.sh
@@ -3,12 +3,34 @@
 export HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
 
-#oc create secret generic registrycreds \
+# find os to ensure correct base64 command syntax
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    mycommand="base64 -w 0"  
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    mycommand="base64 -b 0"
+else
+    printf "\nOS-TYPE is not Linux or Mac, so attempting base64 single-line command. Please investigate if it fails.\n"
+    # For some Mac's we might need to use "/usr/bin/base64 -b 0"
+    mycommand="base64 -w 0"
+fi
+
+# create auth file
+cat <<EOF > ./auth.json
+{
+        "auths": {
+                "$HOST": {
+                        "auth": "$(echo -n $(oc whoami):$(oc whoami -t) | $mycommand)"
+                }
+        }
+}
+EOF
+oc delete secret generic oir-registrycreds 2>/dev/null
+oc create secret generic oir-registrycreds \
+--from-file .dockerconfigjson=./auth.json \
+--type kubernetes.io/dockerconfigjson
+
+#oc create secret generic oir-registrycreds \
 #--from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
 #--type kubernetes.io/dockerconfigjson
-
-oc create secret generic oir-registrycreds \
---from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
---type kubernetes.io/dockerconfigjson
 
 oc extract secret/oir-registrycreds --to=-
