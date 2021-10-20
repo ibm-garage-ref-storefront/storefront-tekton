a7d9d44232c11bd46b920342a8861ed29cd2e6fc
maheshwarishikha <maheshwari.shikha@gmail.com>
2021-10-20 11:36:35 +0530

    Update install-allNamespaces.sh

--- aspects/availability/automated-demo/scripts/install-allNamespaces.sh
+++ aspects/availability/automated-demo/scripts/install-allNamespaces.sh
@@ -13,17 +13,19 @@ OPERATOR_NAME=$1
 FILE=operator-desc.yaml 
 
 # Checking if user is logged-in to OpenShift Cluster
-# echo "\noc whoami"
+# echo "oc whoami"
 oc whoami
 
 if [ $? == 1 ] 
 then
-    echo '\nYou are not logged-in. Login to OpenShift Cluster (oc login)\n'
+    echo 'You are not logged-in. Login to OpenShift Cluster (oc login)'
     exit 0
 fi
 
 OPERATOR_INFO=`oc get packagemanifests -n openshift-marketplace|grep $OPERATOR_NAME`
-echo "\n**Operator Info:\n $OPERATOR_INFO"
+echo "**Operator Info:"
+echo
+echo "$OPERATOR_INFO"
 
 if [ -f "$FILE" ]; then
     rm -f $FILE
@@ -39,23 +41,25 @@ cp subscription.yaml.template $SUBSCRIPTION_FILE
 
 ## replace operator-name
 operator_name=`cat $FILE | grep -E '^Name\b' | cut -d ':' -f 2 | xargs`
-sed -i "" "s/<operator_name>/$operator_name/g" $SUBSCRIPTION_FILE
+sed -i s#operator_name#$operator_name#g $SUBSCRIPTION_FILE
 
 ## replace source-name
 source_name=`cat $FILE | grep 'Catalog Source:' | cut -d ':' -f 2 | xargs`
-sed -i "" "s/<source-name>/$source_name/g" $SUBSCRIPTION_FILE
+sed -i s#source-name#$source_name#g $SUBSCRIPTION_FILE
 
 ## replace channel-name
 channel_name=`cat $FILE | grep 'Default Channel:' | cut -d ':' -f 2 | xargs`
-sed -i "" "s/<channel-name>/$channel_name/g" $SUBSCRIPTION_FILE
+sed -i s#channel-name#$channel_name#g" $SUBSCRIPTION_FILE
 
 ## replace namespace-name
 # echo "namespace name = openshift-operators"
-sed -i "" "s/<namespace_name>/openshift-operators/g" $SUBSCRIPTION_FILE
+sed -i s#namespace_name#openshift-operators#g" $SUBSCRIPTION_FILE
 
-
-echo "\n\n**Installing operator $OPERATOR_NAME: "
+echo
+echo
+echo "**Installing operator $OPERATOR_NAME: "
 oc apply -f $SUBSCRIPTION_FILE
 
 sleep 15
-echo "\nOperator installed successfully."
+echo
+echo "Operator installed successfully."
